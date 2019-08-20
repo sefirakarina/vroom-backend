250ee752b95ceed01f3ec0117a22d618591c6b71
rorobinbin2 <s3553665@student.rmit.edu>
2019-08-21 09:28:40 +1000

    api

--- carShareBackend/routes/api.php
+++ carShareBackend/routes/api.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,33 +10,26 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::group([
-
     //'middleware' => 'api',
     //'namespace' => 'App\Http\Controllers',
     'prefix' => 'auth'
-
 ], function () {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-
 });
-
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-
 Route::get('bookings/customer/{id}', 'BookingController@showCustomerBooking');
-
+Route::put('users/activate/{id}', 'CustomerController@activateCustomer');
+Route::get('users/customers/inactive', 'CustomerController@showInactiveCustomers');
+Route::get('users/customers/active', 'CustomerController@showActiveCustomers');
 Route::resource('bookings', 'BookingController');
 Route::resource('customers', 'CustomerController');
 Route::resource('cars', 'CarController');
 Route::resource('histories', 'HistoryController');
 Route::resource('locations', 'LocationController');
 Route::resource('users', 'UserController');
-
-
